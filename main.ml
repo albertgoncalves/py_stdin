28d15e0637569953f37d456b88796ba77c400c52
albertgoncalves <goncalves.albert@gmail.com>
2019-03-30 08:42:24 -0400

    point free

--- main.ml
+++ main.ml
@@ -1,21 +1,22 @@
 module A = Array
 module L = List
 module S = String
+module Y = Sys
 
 let (|.) (f : 'b -> 'c) (g : 'a -> 'b) : ('a -> 'c) = fun x -> f (g x)
 
-let read () : string =
-    let next_line () : string option =
+let read : (unit -> string) =
+    let line () : string option =
         try Some (input_line stdin) with End_of_file -> None in
     let rec loop (accu : string list) : (string option -> string list) =
         function
-            | Some x -> loop (x::accu) (next_line ())
+            | Some x -> loop (x::accu) (line ())
             | None -> L.rev accu in
-    loop [] (next_line ()) |> S.concat "\n"
+    S.concat "\n" |. loop [] |. line
 
 let main () =
     let lines = read () in
-    let args = (S.concat " " |. A.to_list) Sys.argv in
+    let args = (S.concat " " |. A.to_list) Y.argv in
     L.iter print_endline [args; lines]
 
 let () = main ()
